3190122f376dccbee33834e7e39fe5ad96e9bb2a
KichuTG <kichumon333@outlook.com>
2024-11-16 22:10:28 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,8 +3,8 @@ from os import environ as env
 class Telegram:
     API_ID = int(env.get("API_ID", "28714959"))
     API_HASH = env.get("API_HASH", "c0b9797634090ee3f4c1c56db6c051a7")
-    BOT_TOKEN = env.get("BOT_TOKEN", "8032865598:AAGA9Zetpv7jkJyaPsHu07N2gykNYXRVbzs")
-    BOT_USERNAME = env.get("BOT_USERNAME", "Autocaptgbot")
+    BOT_TOKEN = env.get("BOT_TOKEN", "5558994705:AAG-iw2NrfkLnYxgq8Tv2b9uoAed04BYcLE")
+    BOT_USERNAME = env.get("BOT_USERNAME", "tgpowerbot")
     EMOJIS = [
         "👍", "🙃", "❤", "🔥", 
         "🥰", "👏", "😁", "🤔",
